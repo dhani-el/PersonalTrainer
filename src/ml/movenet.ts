a5fc8d8b95b0c8e0cf85987140b3bcb86b2e4763
daniel ukuhor <dani3lsavag3@gmail.com>
2026-03-21 00:24:06 +0100

    create metro.config.js

--- src/ml/movenet.ts
+++ src/ml/movenet.ts
@@ -1,5 +1,4 @@
 import { loadTensorflowModel } from "react-native-fast-tflite";
-
 export async function loadMovenet() {
   const model = await loadTensorflowModel(
     require("../../assets/models/movenet.tflite"),
